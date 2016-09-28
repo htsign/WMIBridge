1134318ab243e255a43f39ab5dccfe11e3227dd8
htsign <htsign@outlook.com>
2016-09-28 19:54:36 +0900

    create new project

---
+++ WMIClasses/Root/Cimv2/Win32_CurrentTime.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WMIBridge.WMIClasses.Root.Cimv2
+{
+    public class Win32_CurrentTime : WMIObject
+    {
+
+    }
+}
---
+++ WMIClasses/WMIObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WMIBridge.WMIClasses
+{
+    public abstract class WMIObject
+    {
+        public static IEnumerable<T> GetInstances<T>()
+            where T : WMIObject, new()
+        {
+
+        }
+    }
+}
